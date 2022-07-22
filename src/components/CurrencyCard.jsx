4de73505dd133e13689205432b902312635916a8
AsifITk <asifkhanit10@gmail.com>
2022-07-22 15:47:44 +0600

    changed

--- src/components/CurrencyCard.jsx
+++ src/components/CurrencyCard.jsx
@@ -1,9 +1,16 @@
 function CurrencyCard(props) {
-    return (
-        <div className="container-card">
-
-        </div>
-    )
+  return (
+    <div className="container-card">
+      <div className="icon">
+        <img src="./images/logo192" alt="" />
+      </div>
+      <div className="details">
+        <h1 className="unit-price">$49128</h1>
+        <h4 className="name">Bitcoin</h4>
+        <h5 className="last24">last24:</h5>
+      </div>
+    </div>
+  );
 }
 
 export default CurrencyCard;
