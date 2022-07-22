fb7e03b9e4a42a6e915569a181513570bc48665e
ErickPaulSweeney <erickpaulsweeney@gmail.com>
2022-07-22 18:41:14 +0800

    Skeleton5

--- src/components/Holdings.jsx
+++ src/components/Holdings.jsx
@@ -10,10 +10,17 @@ function Holdings(props) {
             {!flag && <div className="buy-some">Go buy some 🚀</div>}
             {flag && Object.keys(props.state.holdings).map(key => {
                 if (props.state.holdings.key > 0) {
+                    let curr = props.state.holdings.key;
+                    let currentValue = props.state.prices.key * curr;
+                    let totalPaid = props.state.holdings.key;
+                    let change = currentValue - totalPaid;
                     return <div className="holdings-card">
-                        <div className="first-row">{pr}</div>
+                        <div className="first-row">{key === 'bit' ? 'Bitcoin' : key === 'eth' ? 'Ethereum' : 'Dogecoin'} - {curr}</div>
+                        <div className="second-row">Total Paid: ${0}, Current Value: ${currentValue}</div>
+                        <div className="third-row" style ={{ color: change > 0 ? 'green' : 'red' }}>P/L: ${change} {change > 0 ? '🚀' : '🔻'}</div>
                     </div>
                 }
+                else return null;
             })}
         </div>
     )
--- src/components/TransactionWindow.jsx
+++ src/components/TransactionWindow.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 function TransactionWindow(props) {
+    let [transaction, setTransaction] = useState('Buy');
+    let [flag, setFlag] = useState(true);
+    let currPrice = props.state.prices[props.input];
+    let max = props.state.currWallet * currPrice;
+
     return (
         <div className="container-window">
-
+            <div className="window-header">{transaction} + {props.input}</div>
+            <div className="window-price">Current Price: {currPrice}</div>
+            <div className="input-row">
+                <input type="number" placeholder="0" onChange={(ev) => {
+                    if (ev.target.value > max) setFlag(false);
+                    else setFlag(true);
+                }}/>
+                <div className="window-max">Max: {max}</div>
+            </div>
+            <div className="buy-row">
+                <input type="radio" id="buy-radio" name="transaction" onClick={() => transaction !== 'Buy' && setTransaction('Sell')}/>
+                <label htmlFor="buy-radio">Buy</label>
+            </div>
+            <div className="sell-row">
+                <input type="radio" id="sell-radio" name="transaction" onClick={() => transaction !== 'Sell' && setTransaction('Buy')}/>
+                <label htmlFor="sell-radio">Sell</label>
+            </div>
+            <button className="transaction-btn" disabled={!flag}>{transaction}</button>
         </div>
     )
 }
