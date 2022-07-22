ab4a7a1d72eb73a9d1daf8d11f52cb1501bf158a
AsifITk <asifkhanit10@gmail.com>
2022-07-22 16:56:01 +0600

    commit

--- src/components/CurrencyPrice.jsx
+++ src/components/CurrencyPrice.jsx
@@ -1,13 +1,13 @@
 import CurrencyCard from "./CurrencyCard";
 
 function CurrencyPrice(props) {
-    return (
-        <div className="container-currency-price">
-            <CurrencyCard />
-            <CurrencyCard />
-            <CurrencyCard />
-        </div>
-    )
+  return (
+    <div className="container-currency-price">
+      <CurrencyCard props={props} coin={props.state.data[0].id} />
+      <CurrencyCard props={props} coin={props.state.data[1].id} />
+      <CurrencyCard props={props} coin={props.state.data[2].id} />
+    </div>
+  );
 }
 
 export default CurrencyPrice;
