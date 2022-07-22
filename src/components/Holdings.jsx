27f3216167bd87e452eda88f4f1a7edd8a95cd74
ErickPaulSweeney <erickpaulsweeney@gmail.com>
2022-07-22 17:46:38 +0800

    Skeleton4

--- src/components/Holdings.jsx
+++ src/components/Holdings.jsx
@@ -1,8 +1,20 @@
 function Holdings(props) {
+    let flag = Object.keys(props.state.holdings).some(key => {
+        if (props.state.holdings.key > 0) return true;
+        else return false;
+    })
+
     return (
         <div className="container-holdings">
             <div className="title-holdings">Current Holdings</div>
-            { !props.holdings && <div className="buy-some">Go buy some 🚀</div> }
+            {!flag && <div className="buy-some">Go buy some 🚀</div>}
+            {flag && Object.keys(props.state.holdings).map(key => {
+                if (props.state.holdings.key > 0) {
+                    return <div className="holdings-card">
+                        <div className="first-row">{pr}</div>
+                    </div>
+                }
+            })}
         </div>
     )
 }
